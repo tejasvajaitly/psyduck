f93f1dbacc7487ed31644fdea7815b491e39ada9
Tejasva Jaitly <tejasvajaitly@gmail.com>
2025-04-15 01:31:56 -0700

    ui updates

--- src/app/transactions.tsx
+++ src/app/transactions.tsx
@@ -10,6 +10,15 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 type Transaction = {
   date: string;
   description: string;
@@ -23,28 +32,37 @@ export default function Transactions({
   transactions: Transaction[];
 }) {
   return (
-    <Table>
-      <TableCaption>A list of transactions.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Date</TableHead>
-          <TableHead>Description</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead className="text-right">Type</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {transactions.map((transaction) => (
-          <TableRow key={transaction.date}>
-            <TableCell className="font-medium">{transaction.date}</TableCell>
-            <TableCell className="truncate max-w-[200px]">
-              {transaction.description}
-            </TableCell>
-            <TableCell>{transaction.amount}</TableCell>
-            <TableCell className="text-right">{transaction.type}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <Card>
+      <CardHeader>
+        <CardTitle>A list of transactions.</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableCaption>A list of transactions.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Date</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead className="text-right">Type</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {transactions.map((transaction) => (
+              <TableRow key={transaction.date}>
+                <TableCell className="font-medium">
+                  {transaction.date}
+                </TableCell>
+                <TableCell className="truncate max-w-[200px]">
+                  {transaction.description}
+                </TableCell>
+                <TableCell>{transaction.amount}</TableCell>
+                <TableCell className="text-right">{transaction.type}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   );
 }
